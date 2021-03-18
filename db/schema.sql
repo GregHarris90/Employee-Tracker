8e5f9004df921df83665c91f2775b4127638cb67
Greg Harris <ghart.productions@gmail.com>
2021-03-17 17:35:40 -0700

    created employees_db & connected my server to it

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,6 @@
 -- Setup database for departments, roles, employees --
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
--- server.js
+++ server.js
@@ -6,8 +6,36 @@ const mysql = require('mysql');
 
 // Setup Connection to db
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'MyCodingCamp90!',
+    database: 'employees_db',
+  });
+
 // Setup start function with inquirer that opens list of options
+
+// const start = () => {}
 
 // Setup first prompt for questions with list
 
 // Setup separate functions with inquirer for each list option
+
+
+
+
+
+
+
+
+
+
+
+// Connect to the mysql server & sql database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected with id ${connection.threadId}`);
+    connection.end();
+    // start();
+  });
